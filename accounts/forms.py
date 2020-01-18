d94043d8e6f2828a305e1a6a5b2c42d086aa4c23
raph941 <raph941@gmail.com>
2020-01-18 13:57:37 +0100

    depoyment

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,5 @@
 import datetime
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Column, Layout, Row, Submit
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
--- peoplestory/forms.py
+++ peoplestory/forms.py
@@ -1,7 +1,5 @@
 import datetime
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Column, Layout, Row, Submit
 from django import forms
 
 from peoplestory.models import Stories, Comment
