ded56c954373d1d26a6c885b99b58c072395a3ff
raph941 <raph941@gmail.com>
2020-02-14 22:49:33 +0100

    settings.py

--- PSP/settings.py
+++ PSP/settings.py
@@ -109,23 +109,23 @@ AUTH_USER_MODEL = 'accounts.User'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#             'default': {
-#             'ENGINE': config('ENGINE'),
-#             'NAME': config('NAME'),
-#             'USER': config('USER'),
-#             'PASSWORD': config('PASSWORD'),
-#             'HOST': config('HOST'),
-#             'PORT': config('PORT'),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+            'default': {
+            'ENGINE': config('ENGINE'),
+            'NAME': config('NAME'),
+            'USER': config('USER'),
+            'PASSWORD': config('PASSWORD'),
+            'HOST': config('HOST'),
+            'PORT': config('PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
