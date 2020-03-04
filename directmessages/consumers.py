a51c39329b5752463f104837e60a35b32750658c
raph941 <raph941@gmail.com>
2020-03-04 10:13:02 +0100

    chat functionality 100% (problems have been solved, tho minor changes can still be made)

--- directmessages/consumers.py
+++ directmessages/consumers.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import datetime
 from django.contrib.auth import get_user_model
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
@@ -21,46 +22,57 @@ class ChatConsumer(AsyncConsumer):
 
     async def websocket_receive(self, event):
         print("recieved", event)
-
         front_data = event.get('text', None)
-        if front_data is not None:
-            loaded_dict_data = json.loads(front_data)
-            sender_pk = loaded_dict_data.get("sender")
-            recipient_pk = loaded_dict_data.get("reciever")
-            message = loaded_dict_data.get("message")
-
-            sender = await self.get_user(sender_pk)
-            recipient = await self.get_user(recipient_pk)
-            instance = await self.send_message(sender, recipient, message)
-            sent_at = str(instance.sent_at)
-
-            '''chat room'''
-            chat_room_obj = await self.get_chat_room(sender, recipient)
+        loaded_dict_data = json.loads(front_data)
+        purpose = loaded_dict_data.get("purpose")
+        
+        if purpose == "input-click-event" or purpose == "hover-event":
+            me = loaded_dict_data.get("me")
+            other_guy = loaded_dict_data.get("other_guy")
+            await self.mark_as_read_bulk(me, other_guy)
+        elif purpose == "fullname-click-event":
+            me = loaded_dict_data.get("me")
+            other_guy = loaded_dict_data.get("other_guy")
+            me = await self.get_user(me)
+            other_guy = await self.get_user(other_guy)
+            chat_room_obj = await self.get_chat_room(me, other_guy)
             self.chat_room_obj = chat_room_obj
-            print(sender, chat_room_obj)
             chat_room = f"chat_{chat_room_obj.id}"
             self.chat_room = chat_room
+            print(me, chat_room_obj)
 
             await self.channel_layer.group_add(
                 chat_room,
                 self.channel_name
             )
+        else:
+            if front_data is not None:
+                sender_pk = loaded_dict_data.get("sender")
+                recipient_pk = loaded_dict_data.get("reciever")
+                message = loaded_dict_data.get("message")
 
-            myResponse = {
-                'message': message,
-                'sender': sender.pk,
-                "recipient": recipient.pk,
-                "sent_at": sent_at
-            }
+                sender = await self.get_user(sender_pk)
+                recipient = await self.get_user(recipient_pk)
+                instance = await self.send_message(sender, recipient, message)
+                sent_at = str(instance.sent_at)
 
-            #broadcasts the message event to be sent
-            await self.channel_layer.group_send(
-                self.chat_room,
-                {
-                    "type": "chat_message",
-                    "text": json.dumps(myResponse)
+                myResponse = {
+                    'message': message,
+                    'sender': sender.pk,
+                    "recipient": recipient.pk,
+                    "sent_at": sent_at
                 }
-            )
+
+                #broadcasts the message event to be sent
+                await self.channel_layer.group_send(
+                    self.chat_room,
+                    {
+                        "type": "chat_message",
+                        "text": json.dumps(myResponse)
+                    }
+                )
+            else:
+                print(event)
         
     async def chat_message(self, event):
         #sends the actual message
@@ -116,3 +128,15 @@ class ChatConsumer(AsyncConsumer):
     @database_sync_to_async
     def get_chat_room(self, user1, user2):
         return ChatRoom.objects.get_or_new(user1, user2)[0]
+
+    @database_sync_to_async
+    def mark_as_read_bulk(self, me, other_guy):
+        '''
+        Note that me, and other_guy are integer fields representing the primary keys of the two fellas repectively
+        '''
+        me = User.objects.get(pk=me)
+        other_guy = User.objects.get(pk=other_guy)
+        unread_messages = Message.objects.all().filter(sender=other_guy, recipient=me, read_at=None)
+        for msg in unread_messages:
+            msg.read_at = datetime.datetime.now()
+            msg.save()
