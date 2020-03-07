a35c2cefb578598264a3230303cf4f96b7013c59
raph941 <raph941@gmail.com>
2020-03-07 06:07:51 +0100

    SETTINGS

--- PSP/settings.py
+++ PSP/settings.py
@@ -225,6 +225,5 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
-        "ROUTING": "PSP.routing.channel_routing",
     },
 }
