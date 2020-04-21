c3f9180badf3442e2a1ec370b2ba089c819cd5d2
raph941 <raph941@gmail.com>
2020-04-21 07:42:25 +0100

    static asset fix

--- PSP/settings.py
+++ PSP/settings.py
@@ -167,7 +167,7 @@ MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
