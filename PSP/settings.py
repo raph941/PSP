ae5c5693dd37e215bd4365bcbf318a702727390a
raph941 <raph941@gmail.com>
2021-03-13 08:46:31 +0100

    chore: Updated config in settings.py

--- PSP/settings.py
+++ PSP/settings.py
@@ -116,18 +116,29 @@ AUTH_USER_MODEL = 'accounts.User'
 
 if config('DATABASE_URL', False):
     DATABASES = {
-    'default': dj_database_url.config(
-        default = config('DATABASE_URL')
-    )
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
+elif config('NAMECHEAP', False):
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': config('DB_ENGINE'),
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('eY1hDroLv@Of'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('5432'),
+        }
     }
 else:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-    }
-
 
 
 # Internationalization
