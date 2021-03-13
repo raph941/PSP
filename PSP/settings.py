231a89a18311b22e061aa3a2fa80d964aac8f865
raph941 <raph941@gmail.com>
2021-03-13 11:51:22 +0100

    chore: Fixed typo

--- PSP/settings.py
+++ PSP/settings.py
@@ -126,7 +126,7 @@ elif config('NAMECHEAP', False):
             'ENGINE': config('DB_ENGINE'),
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
-            'PASSWORD': config('eY1hDroLv@Of'),
+            'PASSWORD': config('DB_PASSWORD'),
             'HOST': config('DB_HOST'),
             'PORT': config('5432'),
         }
@@ -138,7 +138,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
